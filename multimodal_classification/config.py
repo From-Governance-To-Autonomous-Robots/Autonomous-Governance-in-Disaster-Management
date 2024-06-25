971c09e065f99f3c06865f3b10b931efd150de36
juliangdz <juliangeralddcruz@outlook.com>
2024-06-25 09:56:56 +0100

    updated config

--- multimodal_classification/config.py
+++ multimodal_classification/config.py
@@ -3,21 +3,21 @@ import os
 class Config:
     # Paths
     TSV_FILES = [
-        # 'annotations/california_wildfires_final_data.tsv',
-        # 'annotations/hurricane_harvey_final_data.tsv',
+        'annotations/california_wildfires_final_data.tsv',
+        'annotations/hurricane_harvey_final_data.tsv',
         'annotations/hurricane_irma_final_data.tsv',
-        'annotations/hurricane_maria_final_data.tsv'
-        # 'annotations/iraq_iran_earthquake_final_data.tsv',
-        # 'annotations/mexico_earthquake_final_data.tsv',
-        # 'annotations/srilanka_floods_final_data.tsv'
+        'annotations/hurricane_maria_final_data.tsv',
+        'annotations/iraq_iran_earthquake_final_data.tsv',
+        'annotations/mexico_earthquake_final_data.tsv',
+        'annotations/srilanka_floods_final_data.tsv'
     ]
-    COMBINED_DATA_PATH = 'annotations/combined_data.tsv'
-    TRAIN_DATA_PATH = 'annotations/train_data.tsv'
-    VAL_DATA_PATH = 'annotations/val_data.tsv'
-    DATASET_DIR_PATH = '/home/julian/datasets/crisis_mmd/train' 
-    MODEL_SAVE_DIR = 'saved_models'
-    LOG_DIR = 'logs'
-
+    COMBINED_DATA_PATH = 'annotations/combined_data_human.tsv'
+    TRAIN_DATA_PATH = 'annotations/train_data_human.tsv'
+    VAL_DATA_PATH = 'annotations/val_data_human.tsv'
+    DATASET_DIR_PATH = '/home/aaimscadmin/IRP_DATA/CrisisMMD_v2.0'
+    MODEL_SAVE_DIR = 'saved_models_human'
+    LOG_DIR = 'logs_human'
+    
     # Hyperparameters
     BATCH_SIZE = 16
     LEARNING_RATE = 0.0001
@@ -28,15 +28,15 @@ class Config:
     MAX_SEQ_LENGTH = 128
 
     # WandB
-    WANDB_PROJECT = 'multimodal-image-classification'
-    WANDB_NAME = 'training'
+    WANDB_PROJECT = 'multimodal-classification'
+    WANDB_NAME = 'training_human'
 
     # Target Columns for Training
-    TEXT_COLUMNS = ['tweet_text']  # Specify which columns to use as input features
+    TEXT_COLUMNS = ['tweet_text','image_url']  # Specify which columns to use as input features
     IMAGE_COLUMN = 'image_path'
-    TEXT_TARGET_COLUMN = 'text_info'
-    IMAGE_TARGET_COLUMN = 'image_info'
-    DROPPED_COLUMNS = ['tweet_id', 'image_id', 'text_human','text_human_conf', 'image_human','image_human_conf', 'image_damage','image_damage_conf']
+    TEXT_TARGET_COLUMN = 'text_human'
+    IMAGE_TARGET_COLUMN = 'image_human'
+    DROPPED_COLUMNS = ['tweet_id', 'image_id', 'text_info','text_info_conf', 'image_info','image_info_conf', 'image_damage','image_damage_conf']
 
     # Ensure directories exist
     os.makedirs(MODEL_SAVE_DIR, exist_ok=True)
